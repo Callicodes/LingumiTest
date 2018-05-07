c411917a7f16b915423fcc9d1fe543eb95395b55
Aurelius Calliste <callicodes@gmail.com>
2018-05-07 17:09:15 +0100

    Created some Word Objects
    js\students.js

---
+++ js/students.js
@@ -0,0 +1,34 @@
+let words = [ 
+    { 
+     id: 'Happy',
+       hasAlreadyCollected: '6',
+      numberOfTimesLearned: 158,
+       Wordsticker: 14730
+    },
+    {
+      id: 'Nice',
+       hasAlreadyCollected: '5',
+      numberOfTimesLearned: '175',
+       Wordsticker: '16375'
+    },
+    {
+      id: 'Brilliant',
+       hasAlreadyCollected: '7',
+      numberOfTimesLearned: '55',
+       Wordsticker: '2025'
+    },
+  
+    {
+      id: 'Crazy',
+       hasAlreadyCollected: '1',
+      numberOfTimesLearned: '40',
+       Wordsticker: '1950'
+    },
+      {
+          id: 'Moody',
+           hasAlreadyCollected: '2',
+          numberOfTimesLearned: '175',
+           Wordsticker: '16375'
+      }
+  
+  ];
