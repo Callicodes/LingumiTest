0c69e689cb0785be69912f39ca91aa68e8a4fc0b
Aurelius Calliste <callicodes@gmail.com>
2018-05-07 17:13:47 +0100

    Introducing node
    populating the table and creating variables

--- js/index.js
+++ js/index.js
@@ -1,30 +1,20 @@
-var message = '';
-var word;
-var search;
-
-function print(message) {
-  var outputDiv = document.getElementById('output');
-  outputDiv.innerHTML = message;
-}
-
-function getwordReport( word) {
-  var report = '<h2>Word: ' + word.id + '</h2>';
-  report += '<p>hasAlreadyCollected: ' + word.hasAlreadyCollected + '</p>';
-  report += '<p>Wordsticker: ' + word.Wordsticker + '</p>';
-  report += '<p>numberOfTimesLearned: ' + word.numberOfTimesLearned + '</p>';
-  return report;
-}
-
-while (true) {
-  search = prompt('Search word records: type a id [Hello] (or type "quit" to end)');
-  if (search === null || search.toLowerCase() === 'quit') {
-    break;
-  }
-  for (var i = 0; i < words.length; i += 1) {
-    word = words[i];
-    if ( word.id === search ) {
-      message = getwordReport( word );
-      print(message, words);
-    }
-  }
-}
+let search;
+let outputDiv = document.getElementById('output');
+let searchButton = document.getElementById('search');
+let resetButton = document.getElementById('reset');
+let foundStudent = '';
+let count = 0;
+let _INIT = {
+        table({item, key}) {
+            count++;
+            return `
+            <tr id="${key}">
+            <td>${count}</td>
+            <td>${item.id}</tditem>
+            <td>${item.hasAlreadyCollected}</td>
+            <td>${item.numberOfTimesLearned}</td>
+            <td>${item.Wordsticker}</td>
+            <td><button onclick="_INIT.deleteRow(${key})">delete</button></td>
+            </tr>
+            `
+        },
